5a2c71480790ad878ae672f1902e249102cffdd0
guanwenxin <764434043@qq.com>
2023-02-25 10:27:13 +0800

    test plugin

---
+++ src/first-plugin.js
@@ -0,0 +1,22 @@
+// 大驼峰
+const pluginName = 'MyFirstPlugin'
+
+// 给一个类
+// 两个对象
+// compiler
+// compilation
+class ConsoleSomeInfo {
+    // 给apply方法
+    apply(compiler) {
+        compiler.hooks.emit.tapAsync('MyExampleWebpackPlugin', this.handle)
+    }
+
+    handle(compilation, callback) {
+        console.log('这是第一个钩子')
+        console.log(compilation)
+        callback()
+    }
+}
+
+// 导出
+module.exports = ConsoleSomeInfo
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const cusLoder = path.join(__dirname, './src/loader.js')
 const yamlLoader = path.join(__dirname, './src/yaml-loader.js')
+const MyFirstPlugin = require('./src/first-plugin')
 
 module.exports = {
     entry: './test/index.js',
@@ -28,5 +29,6 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [new MyFirstPlugin()]
 }
